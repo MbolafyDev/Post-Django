93abce4c003065883e10da176fe9bb3acf40f8a3
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-08-27 12:54:06 +0300

    Ajouter image dans mon projet Blog

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=250)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="pots", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content =  models.TextField(validators=[MinLengthValidator(10)])
