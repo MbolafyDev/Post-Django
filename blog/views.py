7bf0b0a7ae54ff16c1e70e55667ce30a393bdb32
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-08-28 08:10:03 +0300

    Ajout quelques choses

--- blog/views.py
+++ blog/views.py
@@ -70,6 +70,21 @@ class DetailPageView(View):
 
 
 class ReadLaterView(View):
+
+    def get(self, request):
+        stored_posts =  request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        
+        return render(request, "blog/stored_posts.html", context)
+
     def post(self, request):
         stored_posts =  request.session.get("stored_posts")
 
